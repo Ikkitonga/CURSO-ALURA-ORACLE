51a97ad9a00fc53962ae5420999e53232168db8a
ikkitonga <mcgaston@gmail.com>
2024-01-24 12:15:45 -0300

    creacion html, css, y js

---
+++ desafio encriptador/js.js
@@ -0,0 +1,44 @@
+/* llaves 
+
+la letra e = enter
+la letra i = imes
+la letra a = ai
+la letra o = ober
+la letra u = ufat
+
+requisitos
+--debe funcionar solo con letras minusculas
+--no deben ser utilizados acentos ni caracteres 
+    especiales
+--Debe ser posible convertir una palabra para la 
+    versión encriptada también devolver una palabra 
+    encriptada para su versión origina
+--El resultado debe ser mostrado en la pantalla
+
+Extra
+
+--Un botón que copie el texto encriptado/desencriptado 
+    para la sección de transferencia, o sea que tenga 
+    la misma funcionalidad del ctrl+C o de la opción 
+    "copiar" del menú de las aplicaciones.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
