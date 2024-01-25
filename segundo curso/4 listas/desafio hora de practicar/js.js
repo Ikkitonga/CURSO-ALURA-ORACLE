53033917d965e5bac1323e52982b77b23c5daba3
ikkitonga <mcgaston@gmail.com>
2024-01-25 01:42:40 -0300

    algunos ejercicios resueltos de listas

--- segundo curso/4 listas/desafio hora de practicar/js.js
+++ segundo curso/4 listas/desafio hora de practicar/js.js
@@ -1,24 +1,75 @@
 /* 1 Crea una lista vacía llamada "listaGenerica".*/ 
 
 
+// let listaGenerica = [];
+
+
+
 /* 2 Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion con los 
 siguientes elementos: 'JavaScript', 'C', 'C++', 
 'Kotlin' y 'Python'.*/ 
+
+// let lenguagesDeProgramacion = ["Javascript", "C", "C++", "Kotlin", "Python"];
+
+
 
 
 /* 3 Agrega a la lista "lenguagesDeProgramacion los siguientes elementos: 
 'Java', 'Ruby' y 'GoLang'.*/ 
 
+// let lenguagesDeProgramacion = ["Javascript", "C", "C++", "Kotlin", "Python"]
+//     lenguagesDeProgramacion.push("Java", "Ruby", "GoLang"); 
+// console.log(lenguagesDeProgramacion);
+
+
+
 
 /* 4 Crea una función que muestre en la consola todos los elementos de la lista 
 "lenguagesDeProgramacion. */
+
+// function lenguajes(){
+//     console.log(lenguagesDeProgramacion);
+// }
+
+// lenguajes();
+
+
 
 
 /* 5 Crea una función que muestre en la consola todos los elementos de la lista 
 "lenguagesDeProgramacion en orden inverso.*/ 
 
+// function lenguagesDeProgramacionInversos(){
+   
+//    let inversos = lenguagesDeProgramacion.reverse();
+//    console.log(inversos);
+// }
 
-/* 6 Crea una función que calcule el promedio de los elementos en una lista de números.*/
+// lenguagesDeProgramacionInversos();
+
+
+
+
+/* 6 Crea una función que calcule el promedio de los elementos en una lista de 
+números.*/
+
+
+// function promedioDeArray(){
+//     let suma = 0;
+//     let promedio = 0;
+//     let arrayDeNumeros = [1, 2, 4, 5, 3]
+//     for (let i = 0; i < arrayDeNumeros.length; i++){
+//         suma += arrayDeNumeros[i];
+//         promedio = suma / arrayDeNumeros.length;
+//     }    
+//     console.log(`la suma de los numeros del array es: ${suma}`);
+//     console.log(arrayDeNumeros.length);
+//     console.log(`el promedio de los numeros del array es: ${promedio}`);
+// }
+
+// promedioDeArray()
+
+
 
 
 /* 7 Crea una función que muestre en la consola el número más grande y 
@@ -28,14 +79,14 @@ el número más pequeño en una lista.*/
 /* 8 Crea una función que devuelva la suma de todos los elementos en una lista.*/
 
 
-/* 9 Crea una función que devuelva la posición en la lista donde se encuentra un elemento pasado como 
-parámetro, o -1 si no existe en la lista.*/
+/* 9 Crea una función que devuelva la posición en la lista donde se encuentra un 
+elemento pasado como parámetro, o -1 si no existe en la lista.*/
 
 
-/* 10 Crea una función que reciba dos listas de números del mismo tamaño y devuelva una nueva 
-lista con la suma de los elementos uno a uno.*/
+/* 10 Crea una función que reciba dos listas de números del mismo tamaño y 
+devuelva una nueva lista con la suma de los elementos uno a uno.*/
 
 
-/* 11 Crea una función que reciba una lista de números y devuelva una nueva lista con el cuadrado 
-de cada número.*/
+/* 11 Crea una función que reciba una lista de números y devuelva una nueva 
+lista con el cuadrado de cada número.*/
 
